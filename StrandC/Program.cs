9177ecf8e3f4ff2f49b688e182fe13f5dac44ec8
bakilv <bakilv@310-16>
2026-05-04 14:48:04 +0200

    Class és bekérés

--- StrandC/Program.cs
+++ StrandC/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace StrandC
+﻿using System.Runtime.CompilerServices;
+
+namespace StrandC
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] sr = File.ReadAllLines("strandadatok.txt").Skip(1).ToArray();
+            List<furdo> furdok = new List<furdo>();
+            foreach (string s in sr)
+            {
+                furdok.Add(new furdo(s));
+            }
+
+           
         }
     }
 }
---
+++ StrandC/furdo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrandC
+{
+    public class furdo
+    {
+
+        public string nev { get; private set; }
+        public string cim { get; private set; }
+        public int ar { get; private set; }
+
+        public double vizhofok { get; private set; }
+
+        public furdo(String sor)
+        {
+            string[] adatok = sor.Split(";");
+            nev = adatok[0];
+            cim = adatok[1];
+            ar = int.Parse(adatok[2]);
+            vizhofok = double.Parse(adatok[3]);
+        }
+
+        public string IRSZ()
+        {
+            return cim.Split(" ")[0];
+        }
+
+        public string telepules()
+        {
+            string seged = cim.Split(" ")[1];
+            return seged = seged.Replace(",", "");
+        }
+
+    }
+}
